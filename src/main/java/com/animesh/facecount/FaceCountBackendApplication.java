3ab8dfb87baef4d47cac8cf2e7cc610388135ffd
Animesh Mahata <imanimesh1210@gmail.com>
2025-02-09 20:39:23 +0530

    Project Initialized

---
+++ src/main/java/com/animesh/facecount/FaceCountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.animesh.facecount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceCountBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceCountBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/animesh/facecount/FaceCountBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesh.facecount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceCountBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
