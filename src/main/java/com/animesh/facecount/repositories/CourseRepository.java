c11a912bb33f54987b680fd7425cf0965b3a856d
Animesh Mahata <imanimesh1210@gmail.com>
2025-03-27 22:43:09 +0530

    Added CourseDTO and CourseService

--- src/main/java/com/animesh/facecount/repositories/CourseRepository.java
+++ src/main/java/com/animesh/facecount/repositories/CourseRepository.java
@@ -1,6 +1,5 @@
 package com.animesh.facecount.repositories;
 
-import com.animesh.facecount.dto.CourseDTO;
 import com.animesh.facecount.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
