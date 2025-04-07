f61ce5189081d2612e2f251493d9edd5cd38acec
Animesh Mahata <imanimesh1210@gmail.com>
2025-04-07 20:56:12 +0530

    Bump version to 1.0-SNAPSHOT and clean up unused imports in SecurityConfiguration

--- src/main/java/com/animesh/facecount/config/SecurityConfiguration.java
+++ src/main/java/com/animesh/facecount/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.animesh.facecount.config;
 
-import jakarta.servlet.ServletException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.io.IOException;
 
 /**
  * @author Animesh Mahata
