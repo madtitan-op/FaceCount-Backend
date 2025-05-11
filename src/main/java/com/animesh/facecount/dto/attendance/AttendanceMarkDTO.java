d921cfabd7b841396efad9852d3ddcff38941a90
Animesh Mahata <imanimesh1210@gmail.com>
2025-05-11 12:19:54 +0530

    Documented with Javadoc

--- src/main/java/com/animesh/facecount/dto/attendance/AttendanceMarkDTO.java
+++ src/main/java/com/animesh/facecount/dto/attendance/AttendanceMarkDTO.java
@@ -1,8 +1,15 @@
 package com.animesh.facecount.dto.attendance;
 
+/**
+ * Data Transfer Object for marking attendance.
+ * 
+ * @author Animesh Mahata
+ * @version 1.0
+ */
 public record AttendanceMarkDTO(
         String userId,
 //        Integer courseId,
         String status
 ) {
 }
+
