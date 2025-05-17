a8dee97cad1512b32f785a183c65a6c4cfa3d044
Animesh Mahata <imanimesh1210@gmail.com>
2025-05-17 11:25:39 +0530

    Enhance MyUserDetailsService to support loading users from Student, Faculty, and FaceCountSystem repositories

--- src/main/java/com/animesh/facecount/services/MyUserDetailsService.java
+++ src/main/java/com/animesh/facecount/services/MyUserDetailsService.java
@@ -1,8 +1,9 @@
 package com.animesh.facecount.services;
 
-import com.animesh.facecount.entities.User;
-import com.animesh.facecount.entities.UserPrincipal;
-import com.animesh.facecount.repositories.UserRepository;
+import com.animesh.facecount.entities.*;
+import com.animesh.facecount.repositories.FacultyRepository;
+import com.animesh.facecount.repositories.StudentRepository;
+import com.animesh.facecount.repositories.SystemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,15 +14,52 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepo;
+    private final StudentRepository studentRepository;
+    private final FacultyRepository facultyRepository;
+    private final SystemRepository systemRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        User user = userRepo.findByUserid(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("404! USER NOT FOUND");
+        UserDetails userDetails = loadStudentByUsername(username);
+        if (userDetails != null) {
+            return userDetails;
         }
-        return new UserPrincipal(user);
+
+        userDetails = loadFacultyByUsername(username);
+        if (userDetails != null) {
+            return userDetails;
+        }
+
+        userDetails = loadFCSystemByUsername(username);
+        if (userDetails != null) {
+            return userDetails;
+        }
+
+        throw new UsernameNotFoundException("404! USER NOT FOUND");
     }
+
+    private UserDetails loadStudentByUsername(String username) {
+        Student student = studentRepository.findById(Long.valueOf(username)).orElse(null);
+        if (student != null) {
+            return new StudentPrincipal(student);
+        }
+        return null;
+    }
+
+    private UserDetails loadFacultyByUsername(String username) {
+        Faculty faculty = facultyRepository.findById(Long.valueOf(username)).orElse(null);
+        if (faculty != null) {
+            return new FacultyPrincipal(faculty);
+        }
+        return null;
+    }
+
+    private UserDetails loadFCSystemByUsername(String username) {
+        FaceCountSystem fcSystem = systemRepository.findById(Integer.valueOf(username)).orElse(null);
+        if (fcSystem != null) {
+            return new FaceCountSystemPrincipal(fcSystem);
+        }
+        return null;
+    }
+
 }
